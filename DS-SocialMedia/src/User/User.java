d8d5f2b061a136d9cef336a6900ee6acbb4a5007
Zahra <Zmsmart2020@gmail.com>
2025-01-26 21:32:02 +0330

    User class created

---
+++ DS-SocialMedia/src/User/User.java
@@ -0,0 +1,104 @@
+package src.User;
+
+import java.util.List;
+import java.util.Set;
+
+public class User {
+    private int id;
+    private String name;
+    private String dateOfBirth;
+    private String universityLocation;
+    private String field;
+    private String workplace;
+    private List<String> specialties;
+    private Set<Integer> connections;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUniversityLocation() {
+        return universityLocation;
+    }
+
+    public void setUniversityLocation(String universityLocation) {
+        this.universityLocation = universityLocation;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public void setWorkplace(String workplace) {
+        this.workplace = workplace;
+    }
+
+    public List<String> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(List<String> specialties) {
+        this.specialties = specialties;
+    }
+
+    public Set<Integer> getConnections() {
+        return connections;
+    }
+
+    public void setConnections(Set<Integer> connections) {
+        this.connections = connections;
+    }
+
+    public User(int id, String name, String dateOfBirth, String universityLocation, String field, String workplace, List<String> specialties, Set<Integer> connections) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.universityLocation = universityLocation;
+        this.field = field;
+        this.workplace = workplace;
+        this.specialties = specialties;
+        this.connections = connections;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", universityLocation='" + universityLocation + '\'' +
+                ", field='" + field + '\'' +
+                ", workplace='" + workplace + '\'' +
+                ", specialties=" + specialties +
+                ", connections=" + connections +
+                '}';
+    }
+}
