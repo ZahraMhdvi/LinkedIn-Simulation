c9c0ab8eb011f9fbd63fe0caca9512834f2d6c38
Zahra <Zmsmart2020@gmail.com>
2025-01-28 01:18:18 +0330

    make IndexNode generic

---
+++ DS-SocialMedia/src/DataStructures/BPTree/IndexNode.java
@@ -0,0 +1,43 @@
+package src.DataStructures.BPTree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+
+public class IndexNode<K extends Comparable<K>, T> extends Node<K,T> {
+
+
+    protected ArrayList<Node<K,T>> children;
+
+    public IndexNode(K key, Node<K,T> child0, Node<K,T> child1) {
+        isLeafNode = false;
+        keys = new ArrayList<K>();
+        keys.add(key);
+        children = new ArrayList<Node<K,T>>();
+        children.add(child0);
+        children.add(child1);
+    }
+
+    public IndexNode(List<K> newKeys, List<Node<K,T>> newChildren) {
+        isLeafNode = false;
+
+        keys = new ArrayList<K>(newKeys);
+        children = new ArrayList<Node<K,T>>(newChildren);
+
+    }
+
+
+    public void insertSorted(Entry<K, Node<K,T>> e, int index) {
+        K key = e.getKey();
+        Node<K,T> child = e.getValue();
+        if (index >= keys.size()) {
+            keys.add(key);
+            children.add(child);
+        } else {
+            keys.add(index, key);
+            children.add(index+1, child);
+        }
+    }
+
+}
