c51b4cc80616ef73c78d932c1996749357d71d99
Zahra <Zmsmart2020@gmail.com>
2025-01-29 23:02:16 +0330

    add getter and setter to access table fields

--- DS-SocialMedia/src/Main.java
+++ DS-SocialMedia/src/Main.java
@@ -1,6 +1,7 @@
 import DataStructures.Graph.AdjMapGraph;
 import DataStructures.Table.Table;
 import File.JsonFileHandler;
+import Panel.DataType;
 import Panel.UserPanel;
 import User.User;
 
@@ -10,5 +11,43 @@ public class Main {
     public static void main(String[] args) {
         UserPanel userPanel = new UserPanel();
         userPanel.displayAuthMenu();
+
     }
 }
+
+
+//---------------------------------------------------------
+//این مین برای تست ساخت تیبل برای یوزره پاکش نکن لطفا😘
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        UserPanel userPanel = UserPanel.getUserPanel();
+//
+//        System.out.println("Enter table name:");
+//        String tableName = scanner.nextLine();
+//        userPanel.createTable(tableName, scanner);
+//
+//        System.out.println("Enter table name to insert data:");
+//        tableName = scanner.nextLine();
+//
+//        System.out.println("How many rows do you want to insert?");
+//        int rowCount = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        for (int i = 0; i < rowCount; i++) {
+//            userPanel.insertIntoTable(tableName, scanner);
+//        }
+//
+//        System.out.println("\nTable after insertion:");
+//        userPanel.displayTable(tableName);
+//
+//        System.out.println("\nEnter ID to delete a row:");
+//        int deleteId = scanner.nextInt();
+//        userPanel.deleteRowFromTable(tableName, deleteId);
+//
+//        System.out.println("\nTable after deletion:");
+//        userPanel.displayTable(tableName);
+//    }
+//}
