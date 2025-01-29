c962757afa736d6b3efab3e8ba11f2313d8a98cc
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2025-01-29 17:27:35 +0330

    13th commit.UserPanel class fields are set.

--- DS-SocialMedia/src/Main.java
+++ DS-SocialMedia/src/Main.java
@@ -1,23 +1,12 @@
 import DataStructures.Graph.AdjMapGraph;
 import DataStructures.Table.Table;
 import File.JsonFileHandler;
+import Panel.UserPanel;
 import User.User;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //TODO: move all these to a particular class
-        // to handle constructing default DB and graph then show user's menu
-        List<String> columns = List.of(
-                "id", "name", "dateOfBirth", "universityLocation", "field",
-                "workplace", "specialties", "connections"
-        );
-        Table<Integer, User> userTable = new Table<>("UserTable", columns);
-        JsonFileHandler fileHandler = new JsonFileHandler();
-        AdjMapGraph<User, Integer> usersGraph = new AdjMapGraph<>();
-        fileHandler.constructDefaultTable(userTable);
-        //userTable.displayTable();
-        fileHandler.constructDefaultGraph(usersGraph);
     }
 }
--- DS-SocialMedia/src/Panel/UserPanel.java
+++ DS-SocialMedia/src/Panel/UserPanel.java
@@ -1,13 +1,30 @@
 package Panel;
 
+import DataStructures.Graph.AdjMapGraph;
+import DataStructures.Table.Table;
+import File.JsonFileHandler;
 import User.User;
+
+import java.util.List;
 
 public class UserPanel {
 
     private User currentUser;
     private static UserPanel userPanel;
+    private Table<Integer, User> userTable;
+    private JsonFileHandler fileHandler;
+    private AdjMapGraph<User, Integer> usersGraph;
 
     public UserPanel() {
+        List<String> defaultColumns = List.of(
+                "id", "name", "dateOfBirth", "universityLocation", "field",
+                "workplace", "specialties", "connections"
+        );
+        this.userTable = new Table<>("Default User Table", defaultColumns);
+        this.fileHandler = new JsonFileHandler();
+        this.usersGraph = new AdjMapGraph<>();
+        fileHandler.constructDefaultTable(userTable);
+        fileHandler.constructDefaultGraph(usersGraph);
     }
 
     public static UserPanel getUserPanel() {
@@ -16,4 +33,39 @@ public class UserPanel {
         return userPanel;
     }
 
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public static void setUserPanel(UserPanel userPanel) {
+        UserPanel.userPanel = userPanel;
+    }
+
+    public Table<Integer, User> getUserTable() {
+        return userTable;
+    }
+
+    public void setUserTable(Table<Integer, User> userTable) {
+        this.userTable = userTable;
+    }
+
+    public JsonFileHandler getFileHandler() {
+        return fileHandler;
+    }
+
+    public void setFileHandler(JsonFileHandler fileHandler) {
+        this.fileHandler = fileHandler;
+    }
+
+    public AdjMapGraph<User, Integer> getUsersGraph() {
+        return usersGraph;
+    }
+
+    public void setUsersGraph(AdjMapGraph<User, Integer> usersGraph) {
+        this.usersGraph = usersGraph;
+    }
 }
